b8ed5af32ad64cdb6aaf23cf56349dae2d5b2a3e
i3asm <i3asmmail@gmail.com>
2020-11-11 19:30:02 +0300

    refactor: create forms is a modal instead of a page

--- forms/views.py
+++ forms/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django import template
 from .models import Form, Question, Reply, Answer
+from .forms import FormForm
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def index(request):
     forms = Form.objects.filter(user_id=request.user.id).order_by('-pub_date')
     context = {
@@ -13,20 +17,40 @@ def index(request):
     return render(request, 'forms/index.html', context)
 
 
+@login_required()
 def create(request):
-    forms = Form.objects.order_by('-pub_date').all()
-    context = {
-        'forms': forms
-    }
-    return render(request, 'forms/create.html', context)
+    if request.method == 'POST':
+        form = FormForm(request.POST)
+        if form.is_valid():
+            tmp = Form.objects.create(name=form.cleaned_data['name'], pub_date=timezone.now(),
+                                      user_id=request.user.id).id
+            # return redirect('forms:show', id=tmp)
+            return redirect('forms:index')
+
+    else:
+        form = FormForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'forms/create.html', context)
 
 
 def show(request, id):
     return HttpResponse(Form.objects.get(id=id).question_set.all())
 
 
+@login_required()
 def edit(request, id):
     return HttpResponse(Form.objects.get(id=id))
+
+
+@login_required()
+def delete(request, id):
+    form = Form.objects.get(id=id)
+    print(form)
+    if form.user_id == request.user.id:
+        print(form.delete())
+    return redirect('forms:index')
 
 
 def pages(request):
